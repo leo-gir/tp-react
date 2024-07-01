417f9b0e6d2d09a16329d2fe6e387c04b9c81392
leo-gir <leonel.gironella@gmail.com>
2024-07-01 16:29:19 -0300

    hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,11 +1,13 @@
 import { HeroContaniner } from "./HeroStyles";
-import hero from "../../assets/img/hero.png"
+import hero from "../../assets/img/hero2.png"
+import { HeroBtn } from "./HeroStyles";
 
 function Hero(){
     return(
         <>
             <HeroContaniner>
                 <img src={hero} alt="hero" />
+                <HeroBtn>Ir a la tienda</HeroBtn>
             </HeroContaniner>
         </>
     );
